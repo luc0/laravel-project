a04ce377d7a8ae836a552fd8ebd3c2dd3d160110
Guido Contreras Woda <guiwoda@gmail.com>
2016-11-02 12:04:01 -0300

    Switched WelcomeRoutes to show only Routes

--- app/Http/Routes/WelcomeRoutes.php
+++ app/Http/Routes/WelcomeRoutes.php
@@ -2,28 +2,10 @@
 namespace App\Http\Routes;
 
 use Illuminate\Contracts\Routing\Registrar;
-use Illuminate\Routing\Router;
-use LaravelBA\RouteBinder\RouteBinder;
+use LaravelBA\RouteBinder\Routes;
 
-class WelcomeRoutes implements RouteBinder
+class WelcomeRoutes implements Routes
 {
-    /**
-     * Bind parameters, filters or anything you need to do
-     * with the concrete router here.
-     *
-     * NOTE: If an object that's not an instance (nor an extension) of the concrete
-     * \Illuminate\Routing\Router is bound as the \Illuminate\Contracts\Routing\Registrar
-     * in the Container, **this method will never be called!**
-     *
-     * @param Router $router
-     *
-     * @return void
-     */
-    public function addBindings(Router $router)
-    {
-        //
-    }
-
     /**
      * Add all needed routes to the router.
      *
