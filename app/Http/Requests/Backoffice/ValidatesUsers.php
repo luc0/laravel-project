bbfb8ad0ef5ef368e48aa81c0e92c5829ac4e7b4
Guido Contreras Woda <guiwoda@gmail.com>
2016-11-11 10:45:54 -0300

    Improved backoffice user controller with FormRequests for user creation and edition

---
+++ app/Http/Requests/Backoffice/ValidatesUsers.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Http\Requests\Backoffice;
+
+use Digbang\Security\Users\DefaultUser;
+
+trait ValidatesUsers
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'firstName'   => 'max:255',
+            'lastName'    => 'max:255',
+            'activated'   => 'boolean',
+            'email'       => 'required|email|max:255|unique:'.DefaultUser::class.',email.address',
+            'password'    => 'required_with:password_confirmation|confirmed|min:3',
+            'username'    => 'required|alpha|max:255|unique:'.DefaultUser::class.',username',
+            'roles'       => 'array',
+            'permissions' => 'array',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required'    => trans('backoffice::auth.validation.user.email-required'),
+            'email.unique'      => trans('backoffice::auth.validation.user.user-email-repeated'),
+            'username.required' => trans('backoffice::auth.validation.user.user-username-repeated'),
+            'username.unique'   => trans('backoffice::auth.validation.user.user-username-repeated'),
+            'password.required' => trans('backoffice::auth.validation.user.password-required'),
+        ];
+    }
+}
