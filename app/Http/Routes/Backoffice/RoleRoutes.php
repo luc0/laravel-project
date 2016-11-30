c1b104596b603b1a61e4e7a285ea353faffef97f
Guido Contreras Woda <guiwoda@gmail.com>
2016-11-30 10:09:56 -0300

    Role permission constants

--- app/Http/Routes/Backoffice/RoleRoutes.php
+++ app/Http/Routes/Backoffice/RoleRoutes.php
@@ -18,6 +18,11 @@ class RoleRoutes implements Routes
     const UPDATE = 'backoffice.backoffice-roles.update';
     const DESTROY = 'backoffice.backoffice-roles.destroy';
 
+    const PERMISSION_READ   = 'backoffice.roles.read';
+    const PERMISSION_CREATE = 'backoffice.roles.create';
+    const PERMISSION_UPDATE = 'backoffice.roles.update';
+    const PERMISSION_DELETE = 'backoffice.roles.delete';
+
     /**
      * @var Repository
      */
@@ -39,15 +44,15 @@ class RoleRoutes implements Routes
         $prefix = $this->config->get('backoffice.auth.roles.url', 'backoffice-roles');
 
         $router->group(['prefix' => "backoffice/$prefix", 'middleware' => ['web', 'security:backoffice']], function () use ($router) {
-            $router->get('export',                      ['as' => static::EXPORT,  'uses' => RoleController::class . '@export',  'permission' => 'backoffice.roles.list']);
-            $router->get('/',                           ['as' => static::INDEX,   'uses' => RoleController::class . '@index',   'permission' => 'backoffice.roles.list', 'persistent' => true]);
-            $router->get('create',                      ['as' => static::CREATE,  'uses' => RoleController::class . '@create',  'permission' => 'backoffice.roles.create']);
-            $router->post('/',                          ['as' => static::STORE,   'uses' => RoleController::class . '@store',   'permission' => 'backoffice.roles.create']);
-            $router->get('{'.Bind::SLUG.'}',      ['as' => static::SHOW,    'uses' => RoleController::class . '@show',    'permission' => 'backoffice.roles.read']);
-            $router->get('{'.Bind::SLUG.'}/edit', ['as' => static::EDIT,    'uses' => RoleController::class . '@edit',    'permission' => 'backoffice.roles.update']);
+            $router->get('export',                ['as' => static::EXPORT,  'uses' => RoleController::class . '@export',  'permission' => self::PERMISSION_READ]);
+            $router->get('/',                     ['as' => static::INDEX,   'uses' => RoleController::class . '@index',   'permission' => self::PERMISSION_READ, 'persistent' => true]);
+            $router->get('create',                ['as' => static::CREATE,  'uses' => RoleController::class . '@create',  'permission' => self::PERMISSION_CREATE]);
+            $router->post('/',                    ['as' => static::STORE,   'uses' => RoleController::class . '@store',   'permission' => self::PERMISSION_CREATE]);
+            $router->get('{'.Bind::SLUG.'}',      ['as' => static::SHOW,    'uses' => RoleController::class . '@show',    'permission' => self::PERMISSION_READ]);
+            $router->get('{'.Bind::SLUG.'}/edit', ['as' => static::EDIT,    'uses' => RoleController::class . '@edit',    'permission' => self::PERMISSION_UPDATE]);
             $router->match(['PUT', 'PATCH'],
-                '{'.Bind::SLUG.'}',               ['as' => static::UPDATE,  'uses' => RoleController::class . '@update',  'permission' => 'backoffice.roles.update']);
-            $router->delete('{'.Bind::SLUG.'}',   ['as' => static::DESTROY, 'uses' => RoleController::class . '@destroy', 'permission' => 'backoffice.roles.delete']);
+                '{'.Bind::SLUG.'}',               ['as' => static::UPDATE,  'uses' => RoleController::class . '@update',  'permission' => self::PERMISSION_UPDATE]);
+            $router->delete('{'.Bind::SLUG.'}',   ['as' => static::DESTROY, 'uses' => RoleController::class . '@destroy', 'permission' => self::PERMISSION_DELETE]);
         });
     }
 }
