a206d9c773e4384f229e1ddf448d730504fefb1c
Guido Contreras Woda <guiwoda@gmail.com>
2017-03-02 12:51:19 -0300

    Adapt Feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
